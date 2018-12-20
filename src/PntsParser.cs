4410ad98583e2be5ef6e18e6f8d510ab45234989
bert <btemme@hotmail.com>
2018-12-20 09:48:11 +0100

    typo

--- src/PntsParser.cs
+++ src/PntsParser.cs
@@ -10,7 +10,7 @@ namespace Pnts.Tile
             using (var reader = new BinaryReader(stream))
             {
 
-                // first 4 bytes must be 'b3dm' otherwise its not a b3dm file
+                // first 4 bytes must be 'pnts' otherwise its not a pnts file
                 var magic = Encoding.UTF8.GetString(reader.ReadBytes(4));
                 var version = reader.ReadUInt32();
 
