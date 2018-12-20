4742398abab8d3046753aaf445e305b207adb11a
bert <btemme@hotmail.com>
2018-12-20 12:23:29 +0100

    explore spec

--- src/PntsParser.cs
+++ src/PntsParser.cs
@@ -9,6 +9,8 @@ namespace Pnts.Tile
         {
             using (var reader = new BinaryReader(stream))
             {
+                // spec: https://github.com/AnalyticalGraphicsInc/3d-tiles/tree/master/specification/TileFormats/PointCloud
+                // feature table spec: https://github.com/AnalyticalGraphicsInc/3d-tiles/blob/master/specification/TileFormats/FeatureTable/README.md
 
                 // first 4 bytes must be 'pnts' otherwise its not a pnts file
                 var magic = Encoding.UTF8.GetString(reader.ReadBytes(4));
@@ -23,7 +25,11 @@ namespace Pnts.Tile
                 var featureTableJson = Encoding.UTF8.GetString(featureTableJsonBytes); // "{\"POINTS_LENGTH\":164,\"POSITION\":{\"byteOffset\":0},\"RGB\":{\"byteOffset\":1968},\"RTC_CENTER\":[3830004.5,323597.5,5072948.5]}\n"
 
                 var featureTableBinBytes = reader.ReadBytes((int)featureTableBinByteLength);
+
                 // q: what to do with the featureTableBinBytes ?
+                // `POINTS_LENGTH`| `uint32` | The number of points to render.
+                // dus 164 punten * 3 (x,y,z) * 4 (1 float 4 bytes) = 1968 bytes (en dan beginnen de RGB kleuren)
+                // todo: read first 1968 bytes of featureTableBinBytes and convert to 164 points.
 
                 var pnts = new Pnts() { Magic = magic, Version = (int)version };
                 return pnts;
