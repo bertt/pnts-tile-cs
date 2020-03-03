097b3897ae7dbee7590c4115f564651dff372383
Bert Temme <btemme@hotmail.com>
2020-03-03 10:24:43 +0100

    change unit tests

--- pnts.tile.tests/PntsParserTests.cs
+++ pnts.tile.tests/PntsParserTests.cs
@@ -8,21 +8,39 @@ namespace Tests
     public class PntsParserTests
     {
         Stream pntsfile;
+        string expectedMagicHeader = "pnts";
+        int expectedVersionHeader = 1;
 
         [SetUp]
         public void Setup()
         {
-            const string testfile = "Pnts.Tile.Tests.testfixtures.1-0-1-1.pnts";
-            pntsfile = Assembly.GetExecutingAssembly().GetManifestResourceStream(testfile);
+            const string testfile = "testfixtures/1-0-1-1.pnts";
+            pntsfile = File.OpenRead(testfile);
             Assert.IsTrue(pntsfile != null);
+        }
+
+        [Test]
+        public void TestPointsFile()
+        {
+            // source file: https://github.com/CesiumGS/3d-tiles-samples/blob/master/tilesets/TilesetWithRequestVolume/points.pnts
+            var pntsFile =  File.OpenRead(@"testfixtures/points.pnts");
+            var pnts = PntsParser.ParsePnts(pntsFile);
+            Assert.IsTrue(expectedMagicHeader == pnts.Magic);
+            Assert.IsTrue(expectedVersionHeader == pnts.Version);
+            Assert.IsTrue(pnts.Points.Count == 125000);
+            Assert.IsTrue(pnts.Colors.Count == 125000);
+            Assert.IsTrue(pnts.Points[0].X == (float)-1.1413337);
+            Assert.IsTrue(pnts.Points[0].Y == (float)0.359452039);
+            Assert.IsTrue(pnts.Points[0].Z == (float)-0.361457467);
+            Assert.IsTrue(pnts.Colors[0].R == 182);
+            Assert.IsTrue(pnts.Colors[0].G == 215);
+            Assert.IsTrue(pnts.Colors[0].B == 153);
         }
 
         [Test]
         public void ParsePntsTest()
         {
             // arrange
-            var expectedMagicHeader = "pnts";
-            var expectedVersionHeader = 1;
 
             // act
             var pnts = PntsParser.ParsePnts(pntsfile);
